40b11e9d268124b9e299752f9ea4a0aa499257c4
hgl <hgl199305@163.com>
2018-01-28 10:32:45 +0800

    Initial Commit

---
+++ GetCriticalPath/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GetCriticalPath
+//
+//  Created by zyq on 2018/1/28.
+//  Copyright © 2018年 zyq. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
